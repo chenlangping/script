0379bc9e0c652b265159c13251bfb3a458853930
clp <328566090@qq.com>
2019-04-24 19:56:11 +0800

    add haidilao start script

---
+++ haidilao/start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# start haidilao.py
+
+# if you use python3.6 or higher version,uncomment next line
+# python3 haidilao.py
+#
+# if you use pipenv ,uncomment next line
+# sudo nohup pipenv run python3 haidilao.py runserver > out.log 2>&1 &
