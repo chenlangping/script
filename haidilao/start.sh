bd66bdcd010a0f7d332a220554efc945897fcb55
chenlangping <328566090@qq.com>
2019-04-24 21:16:04 +0800

    change start.sh

--- haidilao/start.sh
+++ haidilao/start.sh
@@ -2,8 +2,12 @@
 
 # start haidilao.py
 
-# if you use python3.6 or higher version,uncomment next line
+# if you use python3.6 or higher version, uncomment next line
 # python3 haidilao.py
-#
-# if you use pipenv ,uncomment next line
+
+# if you use pipenv, uncomment next 5 lines
+# pip install pipenv
+# pipenv --python 3.6
+# pipenv install chardet
+# pipenv install requests
 # sudo nohup pipenv run python3 haidilao.py runserver > out.log 2>&1 &
