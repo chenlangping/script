a11a330972488a9c0210ef0b4e7c856db5dc94b5
chenlangping <328566090@qq.com>
2019-09-04 15:03:05 +0800

    add z to zsh plugins

---
+++ linux/ubuntu/install/install_the_fuck.sh
@@ -0,0 +1,13 @@
+<<!
+ **********************************************************
+ * Author        : clp
+ * Email         : 328566090@qq.com
+ * Last modified : 2019-09-04 14:30
+ * Filename      : install_the_fuck.sh
+ * Description   : install the fuck 
+ * *******************************************************
+!
+#!/bin/bash
+sudo apt update
+sudo apt install python3-dev python3-pip python3-setuptools
+sudo pip3 install thefuck
