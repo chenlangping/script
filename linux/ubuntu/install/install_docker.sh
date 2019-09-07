c3097f793dd57bf0d1b8186ca6e5ec685fcf26d8
chenlangping <328566090@qq.com>
2019-09-07 15:32:32 +0800

    add install docker

---
+++ linux/ubuntu/install/install_docker.sh
@@ -0,0 +1,32 @@
+<<!
+ **********************************************************
+ * Author        : clp
+ * Email         : 328566090@qq.com
+ * Last modified : 2019-09-07 15:23
+ * Filename      : install_docker.sh
+ * Description   : install docker 
+ * *******************************************************
+!
+# first remove old docker version
+sudo apt-get remove docker docker-engine docker-ce docker-ce-cli docker.io
+
+# update the apt package index
+sudo apt-get update
+
+# download script and install
+curl -fsSL get.docker.com -o get-docker.sh
+sudo sh get-docker.sh --mirror Aliyun
+
+# start
+sudo systemctl enable docker
+sudo systemctl start docker
+
+# on Ubuntu 14.04
+# sudo service docker start
+
+# create docker group
+sudo groupadd docker
+sudo usermod -aG docker $USER
+
+# run the test
+# docker run hello_world
