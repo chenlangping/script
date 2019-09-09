1af4f21ae2e3a5e30f6c8f6cb702ef2fc297e117
chenlangping <328566090@qq.com>
2019-09-09 11:53:58 +0800

    add mysql and postgresql

---
+++ linux/ubuntu/install/install_mysql.sh
@@ -0,0 +1,13 @@
+<<!
+ **********************************************************
+ * Author        : clp
+ * Email         : 328566090@qq.com
+ * Last modified : 2019-09-09 11:50
+ * Filename      : install_mysql.sh
+ * Description   : install MySQL
+ * *******************************************************
+!
+#!/bin/zsh
+sudo apt-get install mysql-server
+sudo apt install mysql-client
+sudo apt install libmysqlclient-dev
---
+++ linux/ubuntu/install/install_postgresql.sh
@@ -0,0 +1,11 @@
+<<!
+ **********************************************************
+ * Author        : clp
+ * Email         : 328566090@qq.com
+ * Last modified : 2019-09-09 11:52
+ * Filename      : install_postgresql.sh
+ * Description   : install PostgreSQL
+ * *******************************************************
+!
+#!/bin/zsh
+sudo apt install -y postgresql postgresql-contrib
