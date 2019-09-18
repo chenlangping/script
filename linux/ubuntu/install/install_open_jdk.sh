d7f236febf6c5e0030eceb4bdc7aac949126c41b
chenlangping <328566090@qq.com>
2019-09-18 09:37:41 +0800

    add install openJDK

---
+++ linux/ubuntu/install/install_open_jdk.sh
@@ -0,0 +1,12 @@
+<<!
+ **********************************************************
+ * Author        : clp
+ * Email         : 328566090@qq.com
+ * Last modified : 2019-09-18 09:35
+ * Filename      : install_open_jdk.sh
+ * Description   : 
+ * *******************************************************
+!
+#!/bin/bash
+sudo apt-get install openjdk-8-jdk
+java -version
