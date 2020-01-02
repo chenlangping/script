9e3f208399eac821fdc77be6a2ef1fe1b98c8ff2
chenlangping <328566090@qq.com>
2020-01-02 10:55:15 +0800

    add logging for requests_demo.py

--- docs/python/requests_demo.py
+++ docs/python/requests_demo.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python
-# -- coding:utf-8 --
+# -*- coding:utf-8 -*-
+
 import requests
+import logging
+from bs4 import BeautifulSoup
 
 
-class URL(object):
-    url = "www.example.com"
+class Setting(object):
+    url = "http://www.baidu.com"
+    proxies = {
+        "http": "http://127.0.0.1:1081",
+        "https": "https://127.0.0.1:1081",
+    }
+    timeout = 7
 
 
 class NetworkException(Exception):
@@ -28,6 +36,8 @@ class Cli(object):
         super(Cli, self).__init__()
         self.s = requests.Session()
         self.s.headers = self.headers
+        self.s.timeout = Setting.timeout
+        self.logger = logging.getLogger('logger')
 
     def get(self, url, *args, **kwargs):
         r = self.s.get(url, *args, **kwargs)
@@ -42,9 +52,22 @@ class Cli(object):
         return r
 
 
+def init_logger():
+    logger = logging.getLogger('logger')
+    logger.setLevel(logging.DEBUG)
+    logger_handler = logging.StreamHandler()
+    logger_handler.setLevel(logging.DEBUG)
+    logger_handler.setFormatter(logging.Formatter("[%(asctime)s] [%(levelname)s] %(message)s"))
+    logger.addHandler(logger_handler)
+
+
 def main():
+    init_logger()
     c = Cli()
-    c.get(URL.url)
+    c.get(Setting.url)
+    html_doc = c.get(Setting.url, proxies=Setting.proxies).text
+    soup = BeautifulSoup(html_doc, 'html.parser')
+    print(soup.prettify())
 
 
 if __name__ == '__main__':
