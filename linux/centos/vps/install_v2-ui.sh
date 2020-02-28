aea67a2116f89a5a0dc1322c2c3a112626326c06
chenlangping <328566090@qq.com>
2020-02-29 00:09:49 +0800

    add v2ray-ui script

---
+++ linux/centos/vps/install_v2-ui.sh
@@ -0,0 +1,10 @@
+<<!
+ **********************************************************
+ * Author        : clp
+ * Email         : 328566090@qq.com
+ * Last modified : 2020-02-29 00:06
+ * Filename      : install_v2-ui.sh
+ * Description   : install v2ray
+ * *******************************************************
+!
+bash <(curl -Ls https://blog.sprov.xyz/v2-ui.sh)
