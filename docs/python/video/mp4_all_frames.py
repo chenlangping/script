703533589a5e4f9228232a767da0e96fcde28018
chenlangping <328566090@qq.com>
2020-10-19 14:46:01 +0800

    change cut frames script

--- docs/python/video/mp4_all_frames.py
+++ docs/python/video/mp4_all_frames.py
@@ -1,15 +1,12 @@
-# pip install moviepy
-from moviepy.editor import *
-import os
-
-def extract_frames(movie, times, imgdir):
-    clip = VideoFileClip(movie)
-    for t in times:
-        imgpath = os.path.join(imgdir, '{}.png'.format(t))
-        clip.save_frame(imgpath, t)
-
-movie = 'movie.mp4'
-imgdir = 'frames'
-times = 0.1, 0.63, 0.947, 1.2, 3.8, 6.7
-
-extract_frames(movie, times, imgdir)
+# pip install opencv-python
+import cv2
+# 读取视频
+video = cv2.VideoCapture('test.flv')
+# 逐帧读取，当还有画面时ret为True，frame为当前帧的ndarray对象
+ret, frame = video.read()
+i = 0
+# 循环读取
+while ret:
+    i += 1
+    cv2.imwrite('picture_'+str(i) + '.jpg', frame)
+    ret, frame = video.read()
